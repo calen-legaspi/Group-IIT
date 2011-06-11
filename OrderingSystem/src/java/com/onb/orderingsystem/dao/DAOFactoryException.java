2afa82af6d75b3724febd779345676ceb1453ac7
Lyndon Michael Bibera <lyndon.bibera@orangeandbronze.com>
2011-06-11 18:09:37 +0800

    Added the JdbcDAOFactory implementation.
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@orangeandbronze.com>

---
+++ OrderingSystem/src/java/com/onb/orderingsystem/dao/DAOFactoryException.java
@@ -0,0 +1,8 @@
+package com.onb.orderingsystem.dao;
+
+public class DAOFactoryException extends Exception {
+ 
+    public DAOFactoryException(String message) {
+        super(message);
+    }
+}
--- OrderingSystem/src/java/com/onb/orderingsystem/dao/jdbc/JdbcDAOFactory.java
+++ OrderingSystem/src/java/com/onb/orderingsystem/dao/jdbc/JdbcDAOFactory.java
@@ -1,31 +1,77 @@
 package com.onb.orderingsystem.dao.jdbc;
 
-import com.onb.orderingsystem.dao.CustomerDao;
-import com.onb.orderingsystem.dao.DAOFactory;
-import com.onb.orderingsystem.dao.InventoryItemDao;
-import com.onb.orderingsystem.dao.OrderDao;
-import com.onb.orderingsystem.dao.OrderItemDao;
+import com.onb.orderingsystem.domain.*;
+import com.onb.orderingsystem.dao.*;
+import com.onb.orderingsystem.util.ApplicationEnvironment;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * This implementation 
+ * @see com.onb.orderingsystem.dao.DAOFactory
+ */
 public class JdbcDAOFactory implements DAOFactory {
+    private static DAOFactory instance;
+    
+    private Map<Class,Object> daoInstances;
+    private Connection connection;
+    
+    private JdbcDAOFactory(ApplicationEnvironment env) throws DAOFactoryException {
+        daoInstances = new HashMap<Class, Object>();
+        try {
+            connection = DriverManager.getConnection("jdbc:"+env.dbConnectionDriver+"://"+env.dbUrl+":/"+env.dbName,env.dbUsername,env.dbPassword);
+        } catch(SQLException e) {
+            throw new DAOFactoryException("Failed to establish connection to the database server.");
+        }
+        
+    }
+    
+    /**
+     * 
+     * @param env
+     * @return
+     * @throws DAOFactoryException 
+     */
+    public static DAOFactory getInstance(ApplicationEnvironment env) throws DAOFactoryException {
+        if(instance == null) {
+            instance = new JdbcDAOFactory(env);
+        }
+        return instance;
+    }
 
     @Override
     public CustomerDao getCustomerDao() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(!daoInstances.containsKey(Customer.class)) {
+            daoInstances.put(Customer.class, new JdbcCustomerDao(connection));
+        }
+        return (CustomerDao)daoInstances.get(Customer.class);
     }
 
     @Override
     public InventoryItemDao getInventoryItemDao() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(!daoInstances.containsKey(InventoryItem.class)) {
+            daoInstances.put(InventoryItem.class, new JdbcInventoryItemDao(connection));
+        }
+        return (InventoryItemDao)daoInstances.get(InventoryItem.class);
     }
 
     @Override
     public OrderDao getOrderDao() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(!daoInstances.containsKey(Order.class)) {
+            daoInstances.put(Order.class, new JdbcOrderDao(connection));
+        }
+        return (OrderDao)daoInstances.get(Order.class);
     }
 
     @Override
     public OrderItemDao getOrderItemDao() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(!daoInstances.containsKey(OrderItem.class)) {
+            daoInstances.put(OrderItem.class, new JdbcOrderItemDao(connection));
+        }
+        return (OrderItemDao)daoInstances.get(OrderItem.class);
     }
     
 }
